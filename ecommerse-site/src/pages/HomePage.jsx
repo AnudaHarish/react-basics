5199a4cfb54a8213291fbd6f9c59c4f079aff420
anuda_harish <anudadamsith@gmail.com>
2026-01-30 01:05:39 +0530

    adding routing

--- ecommerse-site/src/pages/HomePage.jsx
+++ ecommerse-site/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import './HomePage.css'
+import './header.css'
 
 export function HomePage(){
   return (
     <>
+      <title>e-commerce prject</title>
       <div className="header">
         <div className="left-section">
           <a href="index.html" className="header-link">
