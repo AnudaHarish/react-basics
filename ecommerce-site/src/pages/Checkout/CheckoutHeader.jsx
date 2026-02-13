54274e19aaffc0075203dbd16f12b93c6aaa7ede
anuda_harish <anudadamsith@gmail.com>
2026-02-13 16:28:09 +0530

    restructuring the code into small components

---
+++ ecommerce-site/src/pages/Checkout/CheckoutHeader.jsx
@@ -0,0 +1,30 @@
+import {productQuantity} from '../../utils/productQuantity';
+import {Link} from 'react-router';
+import './checkout-header.css';
+
+export function CheckoutHeader({cart}) {
+  return (
+    <div className="checkout-header">
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/">
+            <img className="logo" src="images/logo.png" />
+            <img className="mobile-logo" src="images/mobile-logo.png" />
+          </Link>
+        </div>
+
+        <div className="checkout-header-middle-section">
+          Checkout (
+          <Link className="return-to-home-link" to="/">
+            {productQuantity(cart)} items
+          </Link>
+          )
+        </div>
+
+        <div className="checkout-header-right-section">
+          <img src="images/icons/checkout-lock-icon.png" />
+        </div>
+      </div>
+    </div>
+  );
+}
